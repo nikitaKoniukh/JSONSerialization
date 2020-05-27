ab519c651dc98415504bab4d1597d973de04a9d6
Nikita Koniukh <nikita@msapps>
2020-05-27 11:20:00 +0300

    first commit

--- JSONSerialization/ViewController.swift
+++ JSONSerialization/ViewController.swift
@@ -10,11 +10,76 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // Outlets
+    @IBOutlet weak var tableView: UITableView!
+
+    // Properties
+    private let stringUrl = "https://rss.itunes.apple.com/api/v1/us/apple-music/coming-soon/all/100/explicit.json"
+    typealias jsonDictionary = Dictionary<String, Any>
+    var artistsArray = [Artist]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self
+
+        parseJson { result in
+            self.artistsArray = result
+            self.tableView.reloadData()
+        }
     }
 
+    func parseJson(comletion: @escaping([Artist]) -> Void) {
+        var tempArtistsArray = [Artist]()
+        guard let url = URL(string: stringUrl) else { return }
+        let request = NSURLRequest(url: url)
+        let session = URLSession.shared
+        let task = session.dataTask(with: request as URLRequest) { (data, response, error) in
+            if let err = error {
+                print("Error is: \(err.localizedDescription)")
+                return
+            }
+            guard let data = data else { return }
 
+            do {
+                let json = try JSONSerialization.jsonObject(with: data, options: []) as! jsonDictionary
+
+                let feed = json["feed"] as! jsonDictionary
+                let results = feed["results"] as! [jsonDictionary]
+
+                for result in results {
+                    let artistName = result["artistName"] as! String
+                    let releaseDate = result["releaseDate"] as! String
+                    tempArtistsArray.append(Artist(artistName: artistName, releaseDate: releaseDate))
+                }
+                DispatchQueue.main.async {
+                    comletion(tempArtistsArray)
+                }
+            } catch {
+                print(error)
+            }
+        }
+        task.resume()
+    }
+}
+
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return artistsArray.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cellId", for: indexPath)
+        let artist = artistsArray[indexPath.row]
+
+        cell.textLabel?.text = artist.artistName
+        cell.detailTextLabel?.text = artist.releaseDate
+        return cell
+    }
+}
+
+struct Artist {
+    let artistName: String
+    let releaseDate: String
 }
 
