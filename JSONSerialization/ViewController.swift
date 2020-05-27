8bbb04c5a13940f879d8d6e10186bb021b9b1e08
Nikita Koniukh <nikita@msapps>
2020-05-27 10:19:58 +0300

    Initial Commit

---
+++ JSONSerialization/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JSONSerialization
+//
+//  Created by Nikita Koniukh on 27/05/2020.
+//  Copyright © 2020 Nikita Koniukh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
